db65e514fe4980a148e59860a9dbede9d7c685b1
Daniel Rodrigues <inf.daniel@gmail.com>
2026-03-29 13:52:14 -0300

    feat: implement Gin router initialization and basic error handling for user creation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,11 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/daniel-rodrigues/golang-mvc/src/configuration/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-
+	err := rest_err.NewBadRequestError("Invalid json")
+	c.JSON(err.Code, err)
 }
